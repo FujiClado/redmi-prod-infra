cd96bf1ad0a8c82b0c4fcc2f7a120882bf6a380a
fuji <fuji@gmail.com>
2023-12-07 05:03:55 +0000

    ec2 instance created

--- main.tf
+++ main.tf
@@ -91,4 +91,27 @@ resource "aws_security_group" "remote_access" {
   }
 }
 
+# -----------------------------------------------------
+# Creating Ec2 Instance
+# -----------------------------------------------------
+
+resource "aws_instance" "frontend" {
+
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.auth_key.key_name
+  user_data              = file("setup.sh")
+  vpc_security_group_ids = [aws_security_group.http_access.id, aws_security_group.remote_access.id]
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-frontend"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_owner
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 
